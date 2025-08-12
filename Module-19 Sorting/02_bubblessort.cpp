f851c89a0c8de5709a3a0db5c4bf6518a0332dc3
Shreeansh Sahu <shreeyanshxxsahu@gmail.com>
2025-08-12 20:43:56 +0530

    Updated bubblesort

--- Module-19 Sorting/02_bubblessort.cpp
+++ Module-19 Sorting/02_bubblessort.cpp
@@ -5,13 +5,15 @@ using namespace std;
 int main() {
     vector<int> arr = {23, 24, 12, 10, 22};
     int n = arr.size();
-
     for (int i = 0; i < n - 1; i++) {
+        bool flag = true;
         for (int x = 0; x < n - 1 - i; x++) {
             if (arr[x] > arr[x + 1]) {
                 swap(arr[x], arr[x + 1]); // one-line swap
+                flag = false;
             }
         }
+        if (flag) break;
     }
 
     // Print sorted array
