972cf97ebe3b2b1d454ecec739b37593918d83dd
Shreeansh Sahu <shreeyanshxxsahu@gmail.com>
2026-09-01 08:59:18 +0530

    Time: 0 ms (100%), Space: 8.2 MB (83.34%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -4,17 +4,10 @@ bool check(int x ,int y,int m,int n){
 }
 
 int helper (int x ,int y,int m,int n,vector<vector<int>>&dp){
-    if(x==0&&y==0){
-        return 1;
-    }
-    if(!check(x,y,m,n)){
-        return 0;
-    }
-    if(dp[x][y]!=-1){
-        return dp[x][y];
-    }
-    int top = 0;
-    int left = 0;
+    if(x==0&&y==0) return 1;
+    if(!check(x,y,m,n))  return 0;
+    if(dp[x][y]!=-1)  return dp[x][y];
+    int top = 0; int left = 0;
     if(check(x-1,y,m,n)){
         top = helper(x-1,y,m,n,dp);
     }
@@ -25,8 +18,25 @@ int helper (int x ,int y,int m,int n,vector<vector<int>>&dp){
 }
 public:
     int uniquePaths(int m, int n) {
-        vector<vector<int>>dp(m,vector<int>(n,-1));
-        dp[0][0]=0;
-        return helper(m-1,n-1,m,n,dp);
+        vector<int>dp(n,0);
+        dp[0]=1;
+        for (int row = 0; row < m; row++) {
+            for (int col = 0; col < n; col++) {
+                if (row == 0 && col == 0)
+                    continue;
+
+                int top = 0;
+                int left = 0;
+
+                if (row > 0)
+                    top = dp[col];
+
+                if (col > 0)
+                    left = dp[col - 1];
+
+                dp[col] = top + left;
+            }
+        }
+        return dp[n - 1];
     }
 };
