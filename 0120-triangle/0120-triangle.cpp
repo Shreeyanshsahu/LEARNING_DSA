b3146c144e3210497a9e8f42d0eacbe1e83e866a
Shreeansh Sahu <shreeyanshxxsahu@gmail.com>
2026-09-02 21:03:29 +0530

    Time: 0 ms (100%), Space: 12.7 MB (83.93%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -1,31 +1,16 @@
 class Solution {
-    bool check(int x, int y, int m, int n) {
-        return x >= 0 && y >= 0 && x < m && y < n;
-    }
 public:
     int minimumTotal(vector<vector<int>>& triangle) {
-        if(triangle.size()<1) return 0;
-        vector<int>dp(triangle[triangle.size()-1].size(),INT_MAX);
-        dp[0]=triangle[0][0];
-        for(int i =1;i<triangle.size();i++){
-            vector<int>temp(triangle[triangle.size()-1].size(),INT_MAX);
-            for(int j =0;j<triangle[i].size();j++){
-                int minpath = triangle[i][j];
-                if(j==0) {
-                    minpath+=dp[j];
-                }else if(j==triangle[i].size()-1){
-                    minpath+=dp[j-1];
-                }else{
-                    minpath+=min(dp[j-1],dp[j]);
-                }
-                temp[j]=minpath;
+        int n = triangle.size();
+
+        vector<int> dp = triangle[n - 1];
+
+        for(int i = n - 2; i >= 0; i--) {
+            for(int j = 0; j <= i; j++) {
+                dp[j] = triangle[i][j] + min(dp[j], dp[j + 1]);
             }
-            dp=temp;
         }
-        int minpath =INT_MAX;
-        for(int i=0;i<dp.size();i++){
-            minpath=min(dp[i],minpath);
-        }
-        return minpath;
+
+        return dp[0];
     }
 };
